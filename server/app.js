20f38214a00f310f2a17a03f6ef41c7ead0f1067
qnguy <qtn3@njit.edu>
2021-04-25 22:56:38 -0500

    Serializing and Deserializing User Instances to and from The Session

--- server/app.js
+++ server/app.js
@@ -56,6 +56,13 @@ passport.use(strategy);
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
 /*
 const app = express();
 app.use(cors());
