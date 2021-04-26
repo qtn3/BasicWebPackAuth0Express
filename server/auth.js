be754d966cfc5d01384e54f4d5b36d83c4ca13b9
qnguy <qtn3@njit.edu>
2021-04-25 23:00:11 -0500

    Import Packages that are Needed and Load The Environmental Variables

---
+++ server/auth.js
@@ -0,0 +1,11 @@
+// Required External Modules
+const express = require('express');
+
+const router = express.Router();
+const passport = require('passport');
+const querystring = require('querystring');
+
+require('dotenv').config();
+// Routes Definitions
+
+// Module Exports
